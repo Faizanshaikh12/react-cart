593b5589db92ea15eecc6b38d777d71502ae8c68
Faizanshaikh12 <faizanroy000777@gmail.com>
2021-09-21 00:06:10 +0530

    Fetch the products api and the props.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,14 +1,16 @@
-const Product = () => {
-    return(
+const Product = (props) => {
+    console.log(props)
+    const {product} = props;
+    return (
         <div>
-            <img src="/images/peproni.png" alt="Pizza"/>
+            <img src={product.image} alt="Pizza"/>
             <div className="text-center">
-                <h2 className="text-lg font-bold py-2">Havana Special</h2>
-                <span className="bg-gray-200 py-1 rounded-full text-sm px-4">Small</span>
+                <h2 className="text-lg font-bold py-2">{product.name}</h2>
+                <span className="bg-gray-200 py-1 rounded-full text-sm px-4">{product.size}</span>
             </div>
             <span>
                         <div className="flex justify-between items-center mt-4">
-                            <span>₹ 500</span>
+                            <span>₹ {product.price}</span>
                             <button className="bg-yellow-500 py-1 px-4 rounded-full font-bold">ADD</button>
                         </div>
                     </span>
