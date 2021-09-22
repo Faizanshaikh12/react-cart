fee010547e3e4a2a1964d94f1ebe98d405c872c4
Faizanshaikh12 <faizanroy000777@gmail.com>
2021-09-23 00:44:08 +0530

    Cart Page Functionality worked.

--- src/components/Product.js
+++ src/components/Product.js
@@ -9,18 +9,18 @@ const Product = (props) => {
     const addToCart = (event, product) => {
         event.preventDefault();
         let _cart = {...cart};
-        if(!_cart.items){
+        if (!_cart.items) {
             _cart.items = {}
         }
-        if(_cart.items[product._id]){
+        if (_cart.items[product._id]) {
             _cart.items[product._id] += 1;
-        }else{
+        } else {
             _cart.items[product._id] = 1;
         }
-        if(!_cart.totalItems){
+        if (!_cart.totalItems) {
             _cart.totalItems = 0;
         }
-        _cart.totalItems +=1;
+        _cart.totalItems += 1;
         setCart(_cart);
         setIsAdding(true);
         setTimeout(() => {
@@ -39,7 +39,9 @@ const Product = (props) => {
                 <span>
                         <div className="flex justify-between items-center mt-4">
                             <span>₹ {product.price}</span>
-                            <button disabled={isAdding} onClick={(e) => {addToCart(e, product)}}
+                            <button disabled={isAdding} onClick={(e) => {
+                                addToCart(e, product)
+                            }}
                                     className={`${isAdding ? 'bg-green-500' : 'bg-yellow-500'} py-1 px-4 rounded-full font-bold`}>
                                 ADD{isAdding ? 'ED' : ''}</button>
                         </div>
